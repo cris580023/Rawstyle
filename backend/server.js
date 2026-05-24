17dc560f290de7af99cf187edad359d04b6ee3ef
Deiner Pardo <deinerpar21@gmail.com>
2026-05-24 14:36:49 -0500

    Debug archivos en servidor

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
+const fs      = require('fs');
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 const frontendPath = path.join(process.cwd(), 'frontend');
 console.log('process.cwd():', process.cwd());
 console.log('Frontend path:', frontendPath);
+console.log('Archivos en /app:', fs.readdirSync('/app'));
 
 app.use(express.static(frontendPath));
 
