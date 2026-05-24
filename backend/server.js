b71d0b4ec6a34b8fc31615dd5613d42d7714f519
Deiner Pardo <deinerpar21@gmail.com>
2026-05-24 14:15:52 -0500

    Debug rutas frontend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'frontend')));
+const frontendPath = path.join(__dirname, '..', 'frontend');
+console.log('__dirname:', __dirname);
+console.log('Frontend path:', frontendPath);
+
+app.use(express.static(frontendPath));
 
 app.use('/api/auth',      require('./routes/authRoutes'));
 app.use('/api/productos', require('./routes/productosRoutes'));
@@ -17,7 +21,7 @@ app.use('/api/pedidos',   require('./routes/pedidosRoutes'));
 app.use('/api/reportes', require('./routes/reportesRoutes'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+    res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
